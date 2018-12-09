fd2770bd46309a4ae8420e40832c0a173ed9db68
glen <glen_sale@yahoo.com>
2018-12-09 15:47:18 -0800

    it will try to excecute a basic testing

--- WordCounter.Tests/ModelTest/WordCounterTest.cs
+++ WordCounter.Tests/ModelTest/WordCounterTest.cs
@@ -1,71 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
 using WordCounter.Models;
 
 namespace WordCounter.Tests
 {
   [TestClass]
-  public class RepeatCounterTest
+  public class RepeatCounterTests
   {
     [TestMethod]
-    public void CountTheWords_NumberOfWords()
+    public void Constructor_CreatesInstanceOfRepeatCounter_RepeatCounter()
     {
-      //Arrange
-      string input = "Basketball";
-      string output = "I love Basketball";
-      int count = 1;
-
-      // Act
-      int result = RepeatCounter.CountTheWords(input, RepeatCounter.SplitTheText(output));
-
-      //Assert
-      Assert.AreEqual(count, result);
+      RepeatCounter newRepeatCounter = new RepeatCounter("basketball", "");
+      Assert.AreEqual(typeof(RepeatCounter), newRepeatCounter.GetType());
     }
 
-    [TestMethod]
-    public void Wordmatch_Exist()
-    {
-      // Arrange
-      string input = "eat";
-      string output = "I love to cook and eat";
-      int count = 1;
 
-      // Act
-      int result = RepeatCounter.CountTheWords(input, RepeatCounter.SplitTheText(output));
-
-      // Assert
-      Assert.AreEqual(count, result);
-    }
-
-    [TestMethod]
-    public void Wordmatch_NotExist()
-    {
-      // Arrange
-      string input = "Eat";
-      string output = "I love to cook and eat";
-      int count = 0;
-
-      // Act
-      int result = RepeatCounter.CountTheWords(input, RepeatCounter.SplitTheText(output));
-
-      // Assert
-      Assert.AreEqual(count, result);
-    }
-
-    [TestMethod]
-    public void wordToFind_IsNotExisting()
-    {
-      //Arrange
-      string input = "sleep";
-      string output = "This sample is a test";
-      int count = 0;
-
-      // Act
-      int result = RepeatCounter.CountTheWords(input, RepeatCounter.SplitTheText(output));
-
-      // Assert
-      Assert.AreEqual(count, result);
-    }
   }
 }
